e108635e436a5cd20ed090cadf135f065a7aa1ad
jarvis-666 <vm897@snu.edu.in>
2020-04-10 23:48:01 +0530

    Initial commit

---
+++ Week1/find_max_prod.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    def find_max_prod(list_of_numbers):
+        n = len(list_of_numbers)
+        max_index1 = -1
+        for i in range(n):
+            if max_index1 == -1 or list_of_numbers[i] > list_of_numbers[max_index1]:
+                max_index1 = i
+        max_index2 = -1
+        for i in range(n):
+            if i != max_index1 and (max_index2 == -1 or list_of_numbers[i] > list_of_numbers[max_index2]):
+                max_index2 = i
+        return n * list_of_numbers[max_index2]
+
+    number = int(input())
+    list_of_numbers = list(map(int, input().split()))
+    print(find_max_prod(list_of_numbers))
